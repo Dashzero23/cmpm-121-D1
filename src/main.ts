043664d85e493ab75a95774537071e07d45bfbdb
Dashzero23 <114184567+Dashzero23@users.noreply.github.com>
2023-10-10 14:26:38 -0700

    Doing some run check

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-let counter: number = 0;
+let counter = 0;
 
 // Create button
 const monsterButton = document.createElement("button");
@@ -18,17 +18,17 @@ monsterButton.innerHTML = "👾";
 app.append(monsterButton);
 
 const counterReport = document.createElement("div");
-counterReport.innerHTML = `Monsters Slain: ${counter}`;
+counterReport.innerHTML = `Monsters Slain: ${counter} Text Check`;
 app.append(counterReport);
 
 // Increase counter when click button
 monsterButton.addEventListener("click", () => {
   counter++;
-  counterReport.innerHTML = `Monsters Slain: ${counter}`;
+  counterReport.innerHTML = `Monsters Slain: ${counter} Text Check`;
 });
 
 // Increase counter after interval
 setInterval(() => {
     counter++;
-    counterReport.innerHTML = `Monsters Slain: ${counter}`;
+    counterReport.innerHTML = `Monsters Slain: ${counter} Text Check`;
   }, 1000);
