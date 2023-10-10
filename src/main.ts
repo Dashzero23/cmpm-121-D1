ec9674551bcb87b94e3b446c8f87b3514efe7566
Dashzero23 <114184567+Dashzero23@users.noreply.github.com>
2023-10-10 13:00:03 -0700

    Step 1 completed

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,14 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My game";
+const gameName = "Nguyen's game";
 
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const monsterButton = document.createElement("button");
+monsterButton.innerHTML = "👾";
+app.append(monsterButton);
