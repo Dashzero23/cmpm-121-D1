25ec597709f8fb29ffc0cbf6fd415ae68b9c1ced
Dashzero23 <114184567+Dashzero23@users.noreply.github.com>
2023-10-10 13:21:34 -0700

    Step 2 completed

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,19 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+let counter: number = 0;
+
+// Create button
 const monsterButton = document.createElement("button");
 monsterButton.innerHTML = "👾";
 app.append(monsterButton);
+
+const counterReport = document.createElement("div");
+counterReport.innerHTML = `Monsters Slain: ${counter}`;
+app.append(counterReport);
+
+// Increase counter when click button
+monsterButton.addEventListener("click", () => {
+  counter++;
+  counterReport.innerHTML = `Monsters Slain: ${counter}`;
+});
